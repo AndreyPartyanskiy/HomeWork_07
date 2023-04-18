8e09421cbf4cdfdfdbc05a47fe0639d4eb8cc51c
Andrey Partyanskiy <andreypartyanskiy@gmail.com>
2023-04-18 22:32:21 +0300

    first commit

---
+++ Homework_7.1/Homework_7.1.cpp
@@ -0,0 +1,46 @@
+﻿// Homework_7.1.cpp 
+#include <iostream>
+#include <windows.h>
+#define MODE 10
+
+#ifndef  MODE
+#error MODE not defined!!!!!
+#endif
+
+#if MODE ==1
+void add() 
+{
+	int a, b;
+	std::cout << "Введите число 1: ";
+	std::cin >> a;
+	std::cout << "Введите число 2: ";
+	std::cin >> b;
+	std::cout << "Результат сложения: " << a+b << std::endl;
+}
+#endif // MODE ==1
+
+
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+
+	
+
+	#if MODE == 0
+
+		std::cout << "Работаю в режиме тренировки" << std::endl;
+
+	#elif MODE == 1
+
+		std::cout << "Работаю в боевом режиме" << std::endl;
+		add();
+	#else
+
+		std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+	
+	#endif
+
+   
+}
